32b2784d56101709aaf50e5175ad8c4611ae17f3
Nikoleta Zlateva <87802876+Nikoletazl@users.noreply.github.com>
2022-03-18 20:31:56 +0200

    Update models.py

--- web_tools_for_dynamic_websites/tools/tools/web/models.py
+++ web_tools_for_dynamic_websites/tools/tools/web/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    email = models.EmailField()
+
+    name = models.CharField(
+        max_length=25,
+    )
+
+    is_deleted = models.BooleanField(
+        default=False,
+    )
+
+    def __str__(self):
+        return f'{self.pk}: {self.name} with {self.email}'
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        return self.save()
